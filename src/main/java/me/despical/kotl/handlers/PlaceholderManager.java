26a7c3cd9a3a74b7dbe31cc079f946a4ecabe883
Berke Akçen <berkeakcen@gmail.com>
2025-02-01 13:19:37 +0300

    Updated license header

--- src/main/java/me/despical/kotl/handlers/PlaceholderManager.java
+++ src/main/java/me/despical/kotl/handlers/PlaceholderManager.java
@@ -1,6 +1,6 @@
 /*
  * KOTL - Don't let others climb to top of the ladders!
- * Copyright (C) 2024  Berke Akçen
+ * Copyright (C) 2025  Berke Akçen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
