e0dd5267174223cc2102d2dbe1012095a4256e7e
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:57:58 +0300

    Move player commands into the command package.

---
+++ src/main/java/dev/despical/kotl/command/PlayerCommands.java
@@ -0,0 +1,62 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.command;
+
+import dev.despical.commandframework.CommandArguments;
+import dev.despical.commandframework.annotations.Command;
+import dev.despical.kotl.menu.stats.StatsMenu;
+import dev.despical.kotl.user.User;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 05.06.2026
+ */
+public final class PlayerCommands extends CommandCategory {
+
+    @Command(
+        name = "kotl.stats",
+        usage = "/%label% stats [player]",
+        senderType = Command.SenderType.PLAYER
+    )
+    public void statsCommand(User user, CommandArguments arguments) {
+        if (arguments.isArgumentsEmpty()) {
+            new StatsMenu(user).open();
+            return;
+        }
+
+        Player target = arguments.getPlayer(0).orElse(null);
+        if (target != null) {
+            new StatsMenu(user, target).open();
+            return;
+        }
+
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayerIfCached(arguments.getFirst());
+
+        if (offlinePlayer == null) {
+            chatManager.sendMessage(arguments, "no-player-with-that-name");
+            return;
+        }
+
+        new StatsMenu(user, offlinePlayer).open();
+    }
+}
