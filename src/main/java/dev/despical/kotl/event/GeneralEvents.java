9fb16cad5bd2d5eee49f73424e9656ecabf59517
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:57:58 +0300

    Rename general event handling for clearer responsibilities.

---
+++ src/main/java/dev/despical/kotl/event/GeneralEvents.java
@@ -0,0 +1,245 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.event;
+
+import dev.despical.kotl.arena.Arena;
+import dev.despical.kotl.arena.options.ArenaKeys;
+import dev.despical.kotl.game.Game;
+import dev.despical.kotl.option.BooleanOption;
+import dev.despical.kotl.scoreboard.ScoreboardManager;
+import dev.despical.kotl.user.User;
+import dev.despical.kotl.util.ItemUtils;
+import dev.despical.kotl.util.Schedulers;
+import dev.despical.kotl.util.Utils;
+import dev.despical.kotl.util.Var;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.*;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 22.06.2020
+ */
+public class GeneralEvents extends ListenerAdapter {
+
+    private final Map<UUID, Arena> quitPlayers = new HashMap<>();
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        ScoreboardManager.resetPlayerScoreboard(player);
+
+        userManager.createNewUser(player);
+
+        Arena arena = quitPlayers.remove(player.getUniqueId());
+        if (arena == null) {
+            return;
+        }
+
+        Schedulers.runInTheNextTick(() -> {
+            player.teleport(arena.getOption(ArenaKeys.END_LOCATION));
+
+            PlayerInventory inventory = player.getInventory();
+            inventory.clear();
+            inventory.setArmorContents(ItemUtils.EMPTY_ARMORS);
+
+            Utils.restoreSavedPlayerState(player);
+        });
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        ScoreboardManager.resetPlayerScoreboard(player);
+
+        arenaRegistry.getArenas().stream()
+            .map(Arena::getGame)
+            .filter(Objects::nonNull)
+            .forEach(game -> game.getScoreboardManager().removeScoreboard(player));
+
+        User user = userManager.getUser(player);
+        UUID uuid = user.getUUID();
+
+        Arena arena = user.getArena();
+        if (arena != null) {
+            quitPlayers.put(uuid, arena);
+            arenaManager.quitPlayer(user, arena);
+        }
+
+        userManager.removeUser(user);
+
+        plugin.getStatsCacheManager().invalidate(uuid);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDeath(PlayerDeathEvent event) {
+        if (!arenaRegistry.isInArena(event.getEntity())) {
+            return;
+        }
+
+        event.getDrops().clear();
+        event.setDroppedExp(0);
+        event.setShowDeathMessages(false);
+        event.setShouldDropExperience(false);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        User user = userManager.getUser(player);
+        Game game = gameManager.getGame(user);
+
+        if (game == null) {
+            return;
+        }
+
+        Schedulers.runInTheNextTick(() -> game.restoreAfterRespawn(user));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        Arena arena = arenaRegistry.getArena(player);
+        Set<Player> recipients = event.getRecipients();
+
+        boolean separateChat = BooleanOption.SEPARATE_CHAT.value();
+        boolean blockOutsideChat = BooleanOption.BLOCK_OUTSIDE_CHAT.value();
+
+        if (arena == null) {
+            if (blockOutsideChat || separateChat) {
+                arenaRegistry.getArenas().stream()
+                    .map(Arena::getGame)
+                    .flatMap(game -> game.getPlayers().stream())
+                    .forEach(recipients::remove);
+            }
+
+            return;
+        }
+
+        boolean disableChatInGame = BooleanOption.DISABLE_CHAT_IN_GAME.value();
+        if (disableChatInGame) {
+            event.setCancelled(true);
+            chatManager.sendMessage(player, "game.chat-disabled-in-game");
+            return;
+        }
+
+        boolean enableFormatting = BooleanOption.ENABLE_CHAT_FORMATTING.value();
+        if (!enableFormatting && !separateChat) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        Component formattedMessage = chatManager.getMessageComponent(
+            "chat-format",
+            Var.of("%sender%", player.getName()),
+            Var.of("%message%", event.getMessage())
+        );
+
+        if (separateChat) {
+            arena.getGame().broadcastRawComponent(formattedMessage);
+            return;
+        }
+
+        plugin.getServer().broadcast(formattedMessage);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDamage(EntityDamageByEntityEvent event) {
+        cancelIfTaskIsNull(event);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        cancelIfTaskIsNull(event);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player player)) return;
+
+        Arena arena = arenaRegistry.getArena(player);
+        if (arena == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+
+    private <T extends EntityEvent & Cancellable> void cancelIfTaskIsNull(T event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        Arena arena = arenaRegistry.getArena(player);
+        if (arena == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onFallDamage(EntityDamageEvent event) {
+        if (!BooleanOption.DISABLE_FALL_DAMAGE.value()) return;
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        var cause = event.getCause();
+        if (cause != EntityDamageEvent.DamageCause.FALL) return;
+
+        Arena arena = arenaRegistry.getArena(player);
+        if (arena == null) return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+
+        Arena arena = arenaRegistry.getArena(player);
+        if (arena == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onItemPickup(EntityPickupItemEvent event) {
+        if (!BooleanOption.PICK_UP_ITEMS.value()) return;
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        Arena arena = arenaRegistry.getArena(player);
+        if (arena == null) {
+            return;
+        }
+
+        event.getItem().remove();
+        event.setCancelled(true);
+    }
+}
