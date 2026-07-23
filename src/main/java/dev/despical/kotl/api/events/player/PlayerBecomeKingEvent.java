f8bc1c5f0e85527e0264348c92ef67191c9fea77
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:57:57 +0300

    Rename the king event for player-oriented API naming.

---
+++ src/main/java/dev/despical/kotl/api/events/player/PlayerBecomeKingEvent.java
@@ -0,0 +1,55 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.api.events.player;
+
+import dev.despical.kotl.arena.Arena;
+import dev.despical.kotl.game.Game;
+import lombok.Setter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Despical
+ * @since 2.8.1
+ * <p>
+ * Created at 2.02.2024
+ */
+public class PlayerBecomeKingEvent extends PlayerEvent {
+
+    @Setter
+    private boolean cancelled;
+
+    private final Game game;
+
+    public PlayerBecomeKingEvent(Player player, Game game) {
+        super(player);
+        this.game = game;
+    }
+
+    @NotNull
+    public Arena getArena() {
+        return game.getArena();
+    }
+
+    @Override
+    public String toString() {
+        return "player=%s, arena=%s, cancelled=%s"
+            .formatted(getPlayer().getName(), getArena().getId(), cancelled);
+    }
+}
