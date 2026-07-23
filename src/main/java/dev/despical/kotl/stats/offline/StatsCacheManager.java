ee80bd89e4f7a5f4fc7f7d557cccfa255e761d90
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:59:26 +0300

    Add cached offline statistics management.

---
+++ src/main/java/dev/despical/kotl/stats/offline/StatsCacheManager.java
@@ -0,0 +1,67 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.stats.offline;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import dev.despical.kotl.KOTL;
+import org.bukkit.OfflinePlayer;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 04.06.2026
+ */
+public class StatsCacheManager {
+
+    private final KOTL plugin;
+    private final Cache<UUID, OfflineStats> offlineCache;
+
+    public StatsCacheManager(KOTL plugin) {
+        this.plugin = plugin;
+        this.offlineCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .maximumSize(1000)
+            .build();
+    }
+
+    public OfflineStats getStats(OfflinePlayer player) {
+        UUID uuid = player.getUniqueId();
+        OfflineStats cached = offlineCache.getIfPresent(uuid);
+
+        if (cached != null) {
+            return cached;
+        }
+
+        OfflineStats loaded = plugin.getDatabase().loadOfflineData(player);
+        if (loaded == null) {
+            loaded = new OfflineStats(uuid, player.getName());
+        }
+
+        offlineCache.put(uuid, loaded);
+        return loaded;
+    }
+
+    public void invalidate(UUID uuid) {
+        offlineCache.invalidate(uuid);
+    }
+}
