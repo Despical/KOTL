caf16c9a65df02dbf5034a84e228097a0c16495f
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:59:05 +0300

    Add the leaderboard data model.

---
+++ src/main/java/dev/despical/kotl/leaderboard/Leaderboard.java
@@ -0,0 +1,42 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.leaderboard;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 04.06.2026
+ */
+public record Leaderboard<T extends Comparable<T>>(String id, List<LeaderboardEntry<T>> sortedEntries, T fallbackValue) {
+
+    @NotNull
+    public LeaderboardEntry<T> getEntryAtPosition(int pos) {
+        pos -= 1;
+
+        if (pos < 0 || pos >= sortedEntries.size()) {
+            return LeaderboardEntry.empty(fallbackValue);
+        }
+
+        return sortedEntries.get(pos);
+    }
+}
