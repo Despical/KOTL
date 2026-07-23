146fdf16b7cbe9e63b1ee0c8e572f2ec41aeb067
Berke Akçen <berkeakcen@gmail.com>
2026-07-23 10:59:27 +0300

    Add mutable value holder utilities.

---
+++ src/main/java/dev/despical/kotl/util/Var.java
@@ -0,0 +1,74 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.despical.kotl.util;
+
+import dev.despical.kotl.user.User;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.UUID;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 05.06.2026
+ */
+public class Var {
+
+    private static final Var EMPTY_PLAYER = new Var("%player%", "null");
+
+    public final String name;
+    public final Object value;
+
+    private Var(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public static Var of(String name, Object value) {
+        return new Var(name, value);
+    }
+
+    public static Var of(String name, Collection<String> list) {
+        return new Var(name, list.isEmpty() ? Utils.NONE : String.join(", ", list));
+    }
+
+    public static Var ofPlayer(Player player) {
+        if (player == null) {
+            return EMPTY_PLAYER;
+        }
+
+        return new Var("%player%", player.getDisplayName());
+    }
+
+    public static Var ofPlayer(UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+
+        if (player == null) {
+            return EMPTY_PLAYER;
+        }
+
+        return ofPlayer(player);
+    }
+
+    public static Var ofPlayer(User user) {
+        return new Var("%player%", user.getName());
+    }
+}
