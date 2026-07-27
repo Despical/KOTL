902d25fc4d5d2638eb6454cb79d12f360f7983f3
Berke Akçen <berkeakcen@gmail.com>
2026-07-27 13:34:58 +0300

    Document game event lifecycle guarantees

---
+++ src/main/java/dev/despical/kotl/api/events/game/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Provides lifecycle events associated with complete KOTL game instances.
+ * <p>
+ * Game events expose both the runtime game object and its arena. Stop events
+ * are emitted after cleanup and include immutable snapshots for state that is
+ * no longer available through the emptied game membership set.
+ */
+package dev.despical.kotl.api.events.game;
