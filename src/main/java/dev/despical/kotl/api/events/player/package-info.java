90c4bf140f8199525113a59a10b9a80ab7e4eb2b
Berke Akçen <berkeakcen@gmail.com>
2026-07-27 13:34:43 +0300

    Document player event lifecycle guarantees

---
+++ src/main/java/dev/despical/kotl/api/events/player/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * KOTL - Don't let others climb to top of the ladders!
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Provides KOTL events associated with individual players.
+ * <p>
+ * Arena entry and crown claims are cancellable before state changes occur.
+ * Arena departures are informational and expose a leave reason, while
+ * statistic changes allow listeners to cancel or replace the value that will
+ * be stored.
+ */
+package dev.despical.kotl.api.events.player;
