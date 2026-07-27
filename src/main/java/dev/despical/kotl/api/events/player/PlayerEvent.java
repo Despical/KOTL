674901b08cda21f0fef4c2b1b4e6459d874d8c7e
Berke Akçen <berkeakcen@gmail.com>
2026-07-27 13:31:01 +0300

    Document the shared player event contract

--- src/main/java/dev/despical/kotl/api/events/player/PlayerEvent.java
+++ src/main/java/dev/despical/kotl/api/events/player/PlayerEvent.java
@@ -21,34 +21,53 @@ package dev.despical.kotl.api.events.player;
 import dev.despical.kotl.KOTL;
 import dev.despical.kotl.api.events.KOTLEvent;
 import dev.despical.kotl.user.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Base class for KOTL events associated with one Bukkit player.
+ * <p>
+ * Use {@link #getPlayer()} for Bukkit operations and {@link #getUser()} for
+ * KOTL-specific state such as statistics and current arena membership.
+ *
  * @author Despical
  * <p>
  * Created at 05.06.2026
  */
 @Getter
-@AllArgsConstructor
 public abstract class PlayerEvent extends KOTLEvent {
 
-    /** The Bukkit player associated with this event */
+    /**
+     * The Bukkit player associated with this event.
+     */
     @NotNull
     private final Player player;
 
     /**
+     * Constructs a new player event.
+     *
+     * @param player the Bukkit player associated with the event
+     */
+    protected PlayerEvent(@NotNull Player player) {
+        this.player = player;
+    }
+
+    /**
      * Returns the plugin-specific {@link User} for this player.
      *
-     * @return the User object representing the player
+     * @return the KOTL user representing the player
      */
     @NotNull
     public final User getUser() {
         return KOTL.getInstance().getUserManager().getUser(player);
     }
 
+    /**
+     * Returns a compact debug representation containing the player name.
+     *
+     * @return a string containing the event player
+     */
     @Override
     public String toString() {
         return "player=%s".formatted(player.getName());
